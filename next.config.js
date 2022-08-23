30624684fdc9dcef6e92eb579a7544a408b2eb73
laijackylai <66239914+laijackylai@users.noreply.github.com>
2022-08-23 18:09:30 +0800

    try

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  distDir: "build"
+    reactStrictMode: true,
+    swcMinify: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    distDir: "build",
+    images: {
+        loader: 'akamai',
+        path: ''
+    }
 }
 
 module.exports = nextConfig
